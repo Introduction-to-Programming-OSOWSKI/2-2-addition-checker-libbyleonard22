54afbf1da62b4c5030191d971770ed46eabfb364
libbyleonard22 <815989@mystma.org>
2021-09-28 15:08:56 -0500

    3

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 #WRITE YOUR CODE HERE
 def addCheck(x, y, z):
 
-    if (3, 3, 6):
-        return "true"
-    else (3, 4, 6):
-        return "false"
+    if x + y == z:
+        return True
+    else:
+        return False
 
 print(addCheck(x, y, z))
